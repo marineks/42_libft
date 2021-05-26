eca6817889edccf8081d99e33b481113f7c88b84
Marine SANJUAN <msanjuan@e2r5p10.42.fr>
2021-05-26 18:56:11 +0200

    Debug

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,9 @@
 /*   By: msanjuan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 15:38:43 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/05/24 18:11:12 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/05/26 14:54:28 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
 int	ft_atoi(const char *str)
 {
@@ -27,8 +23,7 @@ int	ft_atoi(const char *str)
 	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\r'
 			|| str[i] == '\n' || str[i] == '\v')
 		i++;
-/*	i++; */
-	while (str[i] == '-' || str[i] == '+')
+	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
 			n = -n;
@@ -40,13 +35,4 @@ int	ft_atoi(const char *str)
 		i++;
 	}
 	return (result * n);
-}
-
-int	main(int argc, char **argv)
-{
-	if (argc == 2)
-	{
-		printf("Ma fonction: %d | La vraie: %d\n", ft_atoi(argv[1]), atoi(argv[1]));
-	}
-	return (0);
 }
