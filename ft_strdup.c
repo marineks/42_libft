d553dadb82659feaa33a7bd58b8d655db0db58fa
Marine SANJUAN <msanjuan@e2r1p4.42.fr>
2021-05-27 18:33:20 +0200

    Strdup done

---
+++ ft_strdup.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msanjuan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/27 17:04:29 by msanjuan          #+#    #+#             */
+/*   Updated: 2021/05/27 18:32:12 by msanjuan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strdup(const char *s1)
+{
+	char *copy;
+	
+	copy = (char *)malloc(ft_strlen(s1) + 1 * sizeof(char));
+	if(!copy)
+		return NULL;
+	ft_strlcpy(copy, s1, ft_strlen(s1) + 1);
+
+	return (copy);
+}
