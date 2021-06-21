52c224d7e2739af2b5316a7dbfe59f155a05930f
Marine SANJUAN <marine@sanjuan.eu>
2021-06-21 17:13:01 +0200

    ft_strrchr.c, tests passed

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msanjuan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 15:23:54 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/06/02 11:48:53 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/06/21 15:03:56 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ char *ft_strrchr(const char *s, int c)
 	int i;
 
 	i = ft_strlen((char *)s);
-	if (c == '\0')
+	if ((char)c == '\0')
 		return ((char *)s + i);
 	while (s[--i] && i >= 0)
 	{
-		if (s[i] == c)
+		if (s[i] == (char)c)
 			return ((char *)s + i);
 	}
 	return (NULL);
