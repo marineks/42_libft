de3f9914c866fbef973ab0ffc504f82bd0f93379
Marine SANJUAN <marine@sanjuan.eu>
2021-06-21 16:49:54 +0200

    strchr passed the tripouille test

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msanjuan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 10:55:34 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/05/27 18:29:31 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/06/21 15:07:25 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ char *ft_strchr(const char *s, int c)
 	res = (char *)s;
 	while (*res)
 	{
-		if (*res  == c)
+		if (*res  == (char)c)
 		{
 			return (res);			/* If we find the occurrence */
 		}
 		res++;
 	}
-	if (*res != c)
+	if (*res != (char)c)
 		return (NULL);			/* if we dont find any occurrence in s */
 	return (res); 				/* if c == '\0' and we find it */
 }
